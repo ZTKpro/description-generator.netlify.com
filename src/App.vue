0a83c1d1c4b2378e0ff917b313358cd76813a048
Damian <damianszmurlowork@gmail.com>
2019-08-19 08:36:52 +0200

    added basic struktures

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Generator />
+  </div>
+</template>
+
+<script>
+import Generator from './components/Generator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Generator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Generator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="gen">
+<h1>elo</h1>
+  </div>
+</template>
+
+<script>
+import Generator from './components/Generator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Generator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
